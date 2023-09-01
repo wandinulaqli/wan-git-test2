86b0b6c80e41355790fc86c5e709361351b01e3b
aqlidiningrat <aqli.diningrat07@gmail.com>
2023-09-02 01:34:32 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Coba Git Remote</title>
+  </head>
+  <body>
+    <h1>Hello Word2</h1>
+  </body>
+</html>
